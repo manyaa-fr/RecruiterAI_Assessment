2657c2058cfce5d25ca326ab1803fa08c928d5e1
Manya <mayavi1319@gmail.com>
2026-02-07 05:17:31 +0530

    feat(ui): add scroll-to-top behavior and toast notification system

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import MainLayout from './layouts/MainLayout';
 import Navbar from './components/Navbar';
+import ScrollToTop from './components/ScrollToTop';
+import { Toaster } from '../components/ui/Toaster';
 
 function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop />
       <Navbar />
         <Routes>
           <Route path="/" element={<MainLayout />}>
             <Route index element={<LandingPage />} />
           </Route>
         </Routes>
+        <Toaster />
       </BrowserRouter>
   );
 }
